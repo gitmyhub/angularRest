2accbc410a72c7415fe7a76186f8146e2432291b
ajay <ajay@ajays-MacBook-Pro-2.local>
2018-08-18 14:40:57 -0400

    initial commit

---
+++ src/main/java/com/ajay/oath/practice/angularRest/AngularRestApplication.java
@@ -0,0 +1,12 @@
+package com.ajay.oath.practice.angularRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularRestApplication.class, args);
+	}
+}
